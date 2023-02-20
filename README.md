51899d768b00c681ad7b897b64e36e2b1bf2b318
Antonio Llamas <antonio@allamas.com>
2023-02-20 13:06:37 +0100

    add e-4.js

--- README.md
+++ README.md
@@ -2,18 +2,15 @@
 
 ### Ejercicios
 
-1. [E1](e-1.js) - COMENTARIOS JS y SALIDA EN CONSOLA
-2. [E2](e-2.js) - VALORES y TIPOS
-3. [E3](e-3.js) - VARIABLES Y CONSTANTES
-
-
-
+1. [E1](exercises/e-1.js) - COMENTARIOS JS y SALIDA EN CONSOLA
+2. [E2](exercises/e-2.js) - VALORES y TIPOS
+3. [E3](exercises/e-3.js) - VARIABLES Y CONSTANTES
+4. [E4](exercises/e-4.js) - INPUT Y ASIGNACIÓN
 
 ### Instrucciones
 
-1. En la carpeta `exercises` están los archivos con los contenidos de cada ejercicio. 
+1. En la carpeta `exercises` están los archivos con los contenidos de cada ejercicio.
 2. Dentro de estos archivos `.js` escribirás tus respuestas para cada apartado (`E1.1` `E1.2`).
 3. Abre `index.html` con el navegador y la consola dentro de éste para ver la salida de la ejecución de cada ejercicio.
 4. Los ejercicios se cargarán en consola uno en uno. Por defecto está cargado el ejercicio 1.
-5. Al terminar cada ejercicio debes cambiar el valor de la constante ejercicio dentro del archivo `index.html`. Por ejemplo, sustituye la linea con `const ejercicio = 2;` y refresca el `index.html` en el navegador para ejecutar `./exercises/e-2.js` en consola. 
-
+5. Al terminar cada ejercicio debes cambiar el valor de la constante ejercicio dentro del archivo `index.html`. Por ejemplo, sustituye la linea con `const ejercicio = 2;` y refresca el `index.html` en el navegador para ejecutar `./exercises/e-2.js` en consola.
---
+++ exercises/e-4.js
@@ -0,0 +1,40 @@
+// [LEER 1 minuto] TEORÍA:
+
+// En javascript podemos pedir un valor al usuario con la función prompt("Mensaje")
+
+// La siguiente linea (8) dispara en el navegador una ventana emergente (popup) para solicitar valor al usuario
+
+let user = prompt("¿Escribe tu nombre usuasrio?");
+
+// La ejecución se esperará en la linea 8 hasta que el usuario introduzca el nombre y pulse aceptar.
+
+// Necesito almacenar el valor en una variable para procesarlo
+// En la linea 8 el valor que introduce el usuario se almacena en la variable user antes de pasar a ejecutar la linea 15
+
+console.log("Bienvenido " + user);
+
+/* ---- E1.1. 
+ 
+ Abre el index.html desde el navegador y comprueba que se ejecuta la linea 7. El programa debe abrir un popup con el mensaje indicado pidiendo el valor, introduce en este un nombre y comprueba en la consola que se ejecuta la linea 14 tras pulsar aceptar.
+*/
+
+/* ---- E1.2. 
+
+ Comenta la linea 7 y 14 y descomenta la 28 y 29 antes de ejecutar este ejercicio
+ 
+ Haz los cambios necesiarios en la linea 28 para que se imprima el siguiente mensaje en consola usando el dato del usuario. "Tienes XX años"
+*/
+
+// prompt("¿Escribe tu nombre usuario?");
+// console.log("Tienes " + age + " años");
+
+/* ---- E1.3. 
+
+ Pide un nombre al usuario y almacenalo en una variable nombre
+ Pide un apellido al usuario y almacenalo en la variable apellido
+ Pide la edad al usuario y almacenalo en una variable edad.
+ 
+ Por último suma 1 a la edad del usuario y muestra por consola el siguiente mensaje 
+
+ "Felicidades NOMBRE APELLIDO, hoy cumples NUEVA_EDAD años"
+*/
